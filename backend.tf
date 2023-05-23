9f4e7bd0f44520ea852b9a86e6d01ba115617676
Raghavender Reddy Lonka <96302103+Raghavender-lonka@users.noreply.github.com>
2023-05-23 17:21:53 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         	   = "jenkins-pipeline-bucket1"
-    key              	   = "statefile/terraform.tfstate"
+    key              	   = "raghavender/terraform.tfstate"
     region         	   = "us-east-1"
   }
 }
